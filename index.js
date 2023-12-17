5a9dfc2d0634470d701d1f658a1643a1b5a116d7
Samrat Ahmmed <samratahammed29@gmail.com>
2023-12-17 10:54:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+const uri =
+  "mongodb+srv://samratahammed29:JYsxiMWnYEYHjWSP@cluster0.3o7voea.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const serviceCollection = client.db("BikeDB").collection("service");
+    const userCollection = client.db("BikeDB").collection("users");
+
+    // All services .........
+    app.get("/service", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/singleService/:id", async (req, res) => {
+      const id = req.params.id;
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send("Invalid ObjectId");
+      }
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      if (!result) {
+        return res.status(404).send("Service not found");
+      }
+      res.send(result);
+    });
+
+    app.get("/serviceByEmail/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = await serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/service", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
+    app.put("/serviceUpdate/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      updateService = req.body;
+      updateDoc = {
+        $set: {
+          title: updateService.title,
+          description: updateService.description,
+          price: updateService.price,
+          img: updateService.img,
+          service_area: updateService.area,
+        },
+      };
+      const result = await serviceCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // All user  ........................
+    app.get("/user", async (req, res) => {
+      const cursor = await userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/userRole/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Study is running");
+});
+
+app.listen(port, () => {
+  console.log(`Study Server is running on port ${port}`);
+});
